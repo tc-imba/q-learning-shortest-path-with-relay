bd481b72eade4120682e101c38b1ee99fd39c462
tc-imba <liuyh615@sjtu.edu.cn>
2018-10-05 18:50:35 +0800

    support stations

--- main.py
+++ main.py
@@ -2,111 +2,153 @@ import numpy as np
 import pandas as pd
 import random
 
+stations_required = 1
+
 
 class Node:
-    def __init__(self, name, is_station):
+    def __init__(self, name, is_station: bool):
         self.name = name
         self.is_station = is_station
-        self.path = []
+        # self.path = []
+        self.states = []
+        for i in range(stations_required + 1):
+            self.states.append(State(self, i))
+
+    def add_path(self, p):
+        # self.path.append(p)
+        for i in range(stations_required + 1):
+            self.states[i].add_action(p)
 
 
 class Path:
-    def __init__(self, start, end, cost):
+    def __init__(self, start: Node, end: Node, cost: int):
         self.start = start
         self.end = end
         self.cost = cost
+
+
+class State:
+    def __init__(self, node: Node, stations_count: int):
+        self.node = node
+        self.stations_count = stations_count
+        self.actions_normal = []
+        self.actions_add_station = []
+
+    def add_action(self, p: Path):
+        if self.stations_count < stations_required and p.end.is_station:
+            self.actions_add_station.append(Action(p.end.states[self.stations_count + 1], p))
+        self.actions_normal.append(Action(p.end.states[self.stations_count], p))
+
+    def get_available_actions(self, stations_passed):
+        actions = [] + self.actions_normal
+        for action in self.actions_add_station:
+            if len(stations_passed) < stations_required and action.new_state.node not in stations_passed:
+                actions.append(action)
+        return actions
+
+    def get_best_action(self, stations_passed):
+        actions = self.get_available_actions(stations_passed)
+        action = actions[0]
+        for i in range(1, len(actions)):
+            if actions[i].Q > action.Q:
+                action = actions[i]
+        return action
+
+    def get_random_action(self, stations_passed):
+        actions = self.get_available_actions(stations_passed)
+        return actions[random.randint(0, len(actions) - 1)]
+
+
+class Action:
+    def __init__(self, new_state, path: Path):
+        self.new_state = new_state
+        self.R = -path.cost
         self.Q = 0
-        self.R = -cost
 
 
-node = {}
-path = []
+def __main__():
+    total_episodes = 15000  # Total episodes
+    learning_rate = 0.8  # Learning rate
+    max_steps = 99  # Max steps per episode
+    gamma = 0.95  # Discounting rate
 
-node_df = pd.read_csv('node.csv', header=None)
-for row in node_df.iterrows():
-    name = row[1][0]
-    is_station = row[1][1] == 1
-    node[name] = Node(name, is_station)
+    # Exploration parameters
+    epsilon = 1.0  # Exploration rate
+    max_epsilon = 1.0  # Exploration probability at start
+    min_epsilon = 0.01  # Minimum exploration probability
+    decay_rate = 0.005
 
-path_df = pd.read_csv('path.csv', header=None)
-for row in path_df.iterrows():
-    start = node[row[1][0]]
-    end = node[row[1][1]]
-    cost = row[1][2]
-    p = Path(start, end, cost)
-    path.append(p)
-    start.path.append(p)
-    p = Path(end, start, cost)
-    path.append(p)
-    end.path.append(p)
+    node = {}
+    # path = []
 
-total_episodes = 15000  # Total episodes
-learning_rate = 0.8  # Learning rate
-max_steps = 99  # Max steps per episode
-gamma = 0.95  # Discounting rate
+    node_df = pd.read_csv('node.csv', header=None)
+    for row in node_df.iterrows():
+        name = row[1][0]
+        is_station = row[1][1] == 1
+        node[name] = Node(name, is_station)
 
-# Exploration parameters
-epsilon = 1.0  # Exploration rate
-max_epsilon = 1.0  # Exploration probability at start
-min_epsilon = 0.01  # Minimum exploration probability
-decay_rate = 0.005
+    path_df = pd.read_csv('path.csv', header=None)
+    for row in path_df.iterrows():
+        start = node[row[1][0]]
+        end = node[row[1][1]]
+        cost = row[1][2]
+        p = Path(start, end, cost)
+        # path.append(p)
+        start.add_path(p)
+        p = Path(end, start, cost)
+        # path.append(p)
+        end.add_path(p)
 
-start_node = node[1]
-end_node = node[7]
+    start_node = node[1]
+    end_node = node[7]
 
-for episode in range(total_episodes):
-    total_rewards = 0
-    station_passed = 0
-    state = start_node  # The initial state should be the start node
+    for episode in range(total_episodes):
+        total_rewards = 0
 
-    for step in range(max_steps):
-        tradeoff = random.uniform(0, 1)
-        if tradeoff > epsilon:
-            # Exploitation (max Q in path)
-            action = state.path[0]
-            for p in state.path:
-                if p.Q > action.Q:
-                    action = p
-        else:
-            # Exploration (random Q in path)
-            action = state.path[random.randint(0, len(state.path) - 1)]
+        # The initial state should be the start node with no station passed
+        stations_passed = set()
+        state = start_node.states[len(stations_passed)]
 
-        new_state = action.end
-        reward = action.R
-        done = action.end == end_node
-
-        if new_state.is_station:
-            station_passed += 1
-
-        if done:
-            if station_passed < 1:
-                reward -= 100
+        for step in range(max_steps):
+            tradeoff = random.uniform(0, 1)
+            if tradeoff > epsilon:
+                # Exploitation (max Q in path)
+                action = state.get_best_action(stations_passed)
             else:
-                reward += 10
+                # Exploration (random Q in path)
+                action = state.get_random_action(stations_passed)
 
-        total_rewards += reward
-        new_state_Q = new_state.path[0].Q
-        for p in new_state.path:
-            if p.Q > new_state_Q:
-                new_state_Q = p.Q
-        action.Q += learning_rate * (reward + gamma * new_state_Q - action.Q)
+            new_state = action.new_state
+            reward = action.R
+            done = new_state.node == end_node and new_state.stations_count >= stations_required
 
+            if new_state.node.is_station and new_state.node not in stations_passed:
+                stations_passed.add(new_state.node)
+
+            total_rewards += reward
+            new_state_Q = new_state.get_best_action(stations_passed).Q
+            action.Q += learning_rate * (reward + gamma * new_state_Q - action.Q)
+
+            state = new_state
+
+            if done:
+                break
+
+        epsilon = min_epsilon + (max_epsilon - min_epsilon) * np.exp(-decay_rate * episode)
+
+    stations_passed = set()
+    state = start_node.states[0]
+    for i in range(100):
+        action = state.get_best_action(stations_passed)
+        new_state = action.new_state
+        if new_state.node.is_station and new_state.node not in stations_passed:
+            stations_passed.add(new_state.node)
+        print('%d->%d,Station=%d,R=%d,Q=%f' % (state.node.name, new_state.node.name,
+                                               len(stations_passed), action.R, action.Q))
         state = new_state
-
-        if done:
+        if state.node == end_node and len(stations_passed) >= stations_required:
             break
+    pass
 
-    epsilon = min_epsilon + (max_epsilon - min_epsilon) * np.exp(-decay_rate * episode)
 
-
-state = start_node
-for i in range(10):
-    action = state.path[0]
-    for p in state.path:
-        if p.Q > action.Q:
-            action = p
-    print(action.start.name, action.end.name, action.cost, action.Q)
-    state = action.end
-    if state == end_node:
-        break
-
+__main__()
